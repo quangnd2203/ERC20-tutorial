ec7fec21b733f5bacd3fc9f324c73c08194003f6
quangnd2203 <quangnd.nta@gmail.com>
2023-08-21 01:54:38 +0700

    no message

---
+++ models/network_response.ts
@@ -0,0 +1,30 @@
+class NetworkResponse{
+    
+    constructor(code, message, data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
+
+NetworkResponse.fromErrors = function(code, error){
+    return new NetworkResponse(
+        code = code,
+        message = error,
+        data = null,
+    );
+}
+
+NetworkResponse.success = function(data, message){
+    return new NetworkResponse(
+        code = 200,
+        message = message,
+        data = data,
+    );
+}
+
+module.exports = NetworkResponse;
+
+enum STATUS_CODE{
+    BAD_REQUEST = 401,
+}
---
+++ repositories/smart_contract.ts
@@ -0,0 +1,46 @@
+// require('dotenv').config();
+// const web3 = require('web3')('https://data-seed-prebsc-1-s1.binance.org:8545');
+// const cocoAbi = require('../contracts/coco.json');
+// const vaultAbi = require('../contracts/vault.json');
+
+import { Contract, ContractAbi, ContractMethods, Web3, } from 'web3';
+import * as fs from 'fs';
+import { VAULT_CONTRACT, COCO_CONTRACT } from '../contracts/contracts';
+
+class SmartContractRepo {
+    private web3: Web3;
+
+    private withdrawerPrivateKey: string;
+    private withdrawerAddress: string;
+    private vaultAddress: string;
+    private tokenAddress: string;
+
+    constructor() {
+        this.web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
+        this.withdrawerPrivateKey = process.env.WITHDRAWER_PRIVATE_KEY as string;
+        this.withdrawerAddress = process.env.WITHDRAWER_ADDRESS as string;
+        this.tokenAddress = process.env.COCO_ADDRESS as string;
+        this.vaultAddress = process.env.VAULT_ADDRESS as string;
+    }
+
+    static _instance: SmartContractRepo;
+
+    static async instance(): Promise<SmartContractRepo> {
+        if (!this._instance) {
+            this._instance = new SmartContractRepo();
+        }
+        return this._instance;
+    }
+
+    async withdraw(address: string, amount: number) {
+        this.web3.eth.accounts.wallet.add(this.withdrawerPrivateKey);
+        let value = Web3.utils.toWei(amount.toString(), 'wei');
+        const vaultContract = new this.web3.eth.Contract(VAULT_CONTRACT, this.vaultAddress);
+        let rs = vaultContract.methods.withdraw(value, address).send({
+            from: this.withdrawerAddress,
+            gas: '3000000',
+        });
+        return (await rs).transactionHash;
+    }
+}
+
